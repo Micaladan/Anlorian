d6b04c7a0db10ce475844aea695983368da4afa2
Micaladan <55629315+Micaladan@users.noreply.github.com>
2021-04-26 21:05:27 -0400

    adding google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,16 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8553DDBGM1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8553DDBGM1');
+</script>
+
           <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"

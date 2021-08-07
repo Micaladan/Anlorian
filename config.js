12027823403345cb06f69c063f7edd74ce50cc23
Micaladan <55629315+Micaladan@users.noreply.github.com>
2021-08-06 22:40:17 -0400

    testing p 8080

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
-export const endpoint = `https://api.anlorian.io/api/graphql`;
+export const endpoint = `https://localhost:8080/api/graphql`;
 // Remember no / at the end
 export const prodEndpoint = `https://api.anlorian.io`;
 export const perPage = 4;

c6e92c163561e3124701fcf767abcfc322c0710f
Micaladan <55629315+Micaladan@users.noreply.github.com>
2022-01-30 16:19:27 -0500

    added PleaseSignIn component to sell page, finished auth rules / permissions

--- pages/sell.js
+++ pages/sell.js
@@ -1,10 +1,13 @@
 import CreateProduct from '../components/CreateProduct';
+import PleaseSignIn from '../components/PleaseSignIn';
 
 export default function sell() {
   return (
     <div>
       <h1 className="heading-primary heading-primary--main">Sell Page</h1>
-      <CreateProduct />
+      <PleaseSignIn>
+        <CreateProduct />
+      </PleaseSignIn>
     </div>
   );
 }
